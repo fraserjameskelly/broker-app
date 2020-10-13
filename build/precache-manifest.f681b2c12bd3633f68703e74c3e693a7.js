f8e196195cb9ac058d8c38bdd1ba42e0740cef10
Fraser Kelly <fkelly@blue2.co.uk>
2020-10-14 00:48:28 +0100

    Build

---
+++ build/precache-manifest.f681b2c12bd3633f68703e74c3e693a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640256eafa09c2c64442af15337e0412",
+    "url": "/index.html"
+  },
+  {
+    "revision": "555aa15cf86e22532733",
+    "url": "/static/css/main.603eb0e6.chunk.css"
+  },
+  {
+    "revision": "b1795f9b5953abaaf352",
+    "url": "/static/js/2.fb91bee6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fb91bee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "555aa15cf86e22532733",
+    "url": "/static/js/main.c1f56d3a.chunk.js"
+  },
+  {
+    "revision": "a0d4c7c2ed27bc8af66b",
+    "url": "/static/js/runtime-main.30edea87.js"
+  }
+]);
